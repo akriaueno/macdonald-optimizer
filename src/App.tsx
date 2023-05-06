4085d1cf4426d5ee3c2d7f41aa69209ab13f8622
akriaueno <akiraueno@outlook.com>
2023-05-06 12:10:35 +0900

    add objective and constraints element

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,30 @@ function App() {
     fetchData();
   }, []);
 
+  const runIP = () => {
+    console.log(menu, nutorient);
+  };
+
   return (
     <>
       <h1>McDonald's optimizer</h1>
       <div className="card">
-        <RunButton onClick={() => {}}></RunButton>
+        <div>
+          <label htmlFor="objective">目的:</label>
+          <select name="objective" id="objective">
+            <option value="price">料金</option>
+            <option value="weight">総重量</option>
+          </select>
+          の
+          <select name="min-max" id="min-max">
+            <option value="minimize">最小化</option>
+            <option value="maximize">最大化</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="constraint">制約条件:</label>
+        </div>
+        <RunButton onClick={runIP}></RunButton>
       </div>
     </>
   );
