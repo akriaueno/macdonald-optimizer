f54b16ae04afc7f0760fc5318902ce0615deb7d5
akiraueno <akky@akky.me>
2024-03-29 01:15:57 +0900

    rm all

--- src/components/RunButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {
-  onClick: () => void;
-}
-
-export const RunButton: React.FC<Props> = ({ onClick }) => {
-  return <button onClick={onClick}>実行</button>;
-};
